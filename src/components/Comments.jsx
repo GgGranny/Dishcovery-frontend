d97dd132915c85c8f80514098635adcb7480a36d
GgGranny <jonjina7@gmail.com>
2025-11-27 14:04:00 +0545

    comments modified

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -6,8 +6,19 @@ import axios from "axios";
 
 const Comments = () => {
     const [comments, setComments] = useState([]); // <-- FIX: start with empty array
-
+    const [videoId, setVideoId] = useState('cf69162d-35bf-4021-bf88-e04e6094bca8');
+    const [videoData, setVideoData] = useState(null);
     useEffect(() => {
+        axios.get(`http://localhost:8080/api/v1/videos/stream/segment/${videoId}/master.m3u8`, {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        })
+            .then(res => {
+                setVideoData(res.config.url)
+                console.log(res)
+            })
+            .catch(error => console.error(error))
         axios.get("http://localhost:8080/api/comments/c1/get-comments/2", {
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("token")
@@ -27,7 +38,7 @@ const Comments = () => {
         return (
             <div style={{ marginLeft: "15px" }} className="flex flex-row gap-1">
                 <div className="w-5 h-5 border shrink-0 rounded-full overflow-hidden">
-                    <img src={`data:image/jpeg;base64,` + comment?.user?.profilePicture} className="w-full h-full" alt="profile" />
+                    <img src={comment?.user?.profilePicture} className="w-full h-full" alt="profile" />
                 </div>
                 <div>
                     <p>{comment.user?.username}</p>
@@ -48,7 +59,7 @@ const Comments = () => {
     return (
         <div className="border border-red-400 flex flex-col h-screen">
             <div className="h-[50%]">
-                <VideoPlayer />
+                <VideoPlayer src={videoData} />
             </div>
             <div className="flex flex-col">
                 Recipe
