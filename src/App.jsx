2b59fb835491b807651d119e6be8e60593249997
GgGranny <jonjina7@gmail.com>
2026-02-03 08:38:03 +0545

    new: intigrating pdf

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,7 @@ import PaymentSuccess from "./components/PaymentSuccess";
 import Payments from "./components/Payment";
 import Chat from "./frontend/afterlogin/Chat";
 import CommunityChat from "./frontend/afterlogin/CommunityChat";
+import Pdf from "./frontend/Pdf";
 
 
 function App() {
@@ -91,7 +92,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/communities" element={<Communities />} />
         <Route path="/chat" element={<Chat />} />
-
+        <Route path="/pdf" element={<Pdf name="Abhishek" />} />
         {/* 🔐 Protected Routes (NO main page needed) */}
         <Route element={<ProtectedRoute />}>
           <Route path="/homepage" element={<Homepage />} />
